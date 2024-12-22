4351e4e8e18271aed5b906eee598855fab06c2ed
quentin <quentin.franssen@student.unamur.be>
2024-12-22 23:48:55 +0100

    Using DOxygen approach to document Iteration0 code

--- Iteration_0/main.c
+++ Iteration_0/main.c
@@ -5,30 +5,45 @@
 #include <assert.h>
 #include <unistd.h>
 
-#define NUM_MEASURES 10     // Nombre de mesures à prendre
-#define LED_PIN 25          // Pin GPIO pour la LED
-#define GPIO_OUT 0
-#define TEMP_SENSOR_ADDR 0x48 // Adresse I2C du capteur de température (exemple)
+/// \brief Number of temperature measurements to perform.
+#define NUM_MEASURES 10
 
-// Variables pour stocker les mesures et les calculs
+/// \brief GPIO pin for the LED.
+#define LED_PIN 25
+
+/// \brief I2C address of the temperature sensor (example).
+#define TEMP_SENSOR_ADDR 0x48
+
+/// \brief Array to store temperature values.
 float temperature_values[NUM_MEASURES];
+/// \brief Current number of temperature measurements taken.
 uint8_t measure_count = 0;
+/// \brief Global counter for executed tasks.
 uint8_t global_taskcounter = 0;
 
-// Increment Task Counter and show the task name.
+/**
+ * \brief Increments the global task counter and prints the name of the last executed task.
+ * 
+ * \param taskName Name of the executed task.
+ */
 void incrementTaskCounter(const char *taskName) {
     global_taskcounter++;
     printf("%d tasks executed so far. Last task: %s\n", global_taskcounter, taskName);
 }
 
-// Initialisation des périphériques
+/**
+ * \brief Initializes the necessary peripherals (simulated for Linux).
+ */
 void init_peripherals() {
     printf("Initializing peripherals (simulated for Linux)...\n");
 }
 
-// Fonction pour lire la température
+/**
+ * \brief Reads a simulated temperature value from the sensor.
+ * 
+ * \return The simulated temperature in degrees Celsius.
+ */
 float read_temperature() {
-    // Set the range
     float min = -40.0f;
     float max = 60.0f;
 
@@ -38,7 +53,11 @@ float read_temperature() {
     return temp_1d;
 }
 
-// Fonction pour calculer la moyenne des températures
+/**
+ * \brief Calculates the average of the stored temperature values.
+ * 
+ * \return The average temperature value, or 0 if no measurements have been taken.
+ */
 float average() {
     float sum = 0.0;
     for (uint8_t i = 0; i < measure_count; i++) {
@@ -47,7 +66,11 @@ float average() {
     return (measure_count > 0) ? (sum / measure_count) : 0;
 }
 
-// Fonction pour allumer la LED X fois
+/**
+ * \brief Simulates blinking an LED a specified number of times.
+ * 
+ * \param times Number of times to blink the LED.
+ */
 void blink_led(uint8_t times) {
     printf("Simulating blinking LED %d times...\n", times);
     for (uint8_t i = 0; i < times; i++) {
@@ -58,12 +81,18 @@ void blink_led(uint8_t times) {
     }
 }
 
-// Fonction pour simuler un sleep
+/**
+ * \brief Simulates a sleep for the specified number of milliseconds.
+ * 
+ * \param ms Time to sleep in milliseconds.
+ */
 void sleep_ms(uint32_t ms) {
-    usleep(ms * 1000);  // Convertit ms en µs
+    usleep(ms * 1000);  // Converts milliseconds to microseconds
 }
 
-// Task: Get the current value of temperature from the sensor.
+/**
+ * \brief Task to retrieve the current temperature value from the sensor.
+ */
 void runTempTask() {
     if (measure_count < NUM_MEASURES) {
         temperature_values[measure_count] = read_temperature();
@@ -72,7 +101,9 @@ void runTempTask() {
     }
 }
 
-// Task: Compute the average of temperatures retrieves in volatile memory.
+/**
+ * \brief Task to compute the average of the temperatures stored in memory.
+ */
 void computeAvgTempTask() {
     if (measure_count == NUM_MEASURES) {
         float average_temp = average();
@@ -81,16 +112,20 @@ void computeAvgTempTask() {
     }
 }
 
-// Task: send Result, simulated by the bliking of the LED based on the amount of measure retrieved.
+/**
+ * \brief Task to simulate sending the result by blinking the LED a number of times equal to the collected measurements.
+ */
 void sendResultTask() {
     if (measure_count == NUM_MEASURES) {
         blink_led(measure_count);
-        measure_count = 0;  // Réinitialiser le compteur de mesures
+        measure_count = 0;  // Reset the measurement counter
         incrementTaskCounter("sendResultTask");
     }
 }
 
-
+/**
+ * \brief Scheduler function to manage task execution in sequence.
+ */
 void task_scheduler() {
     runTempTask();
     sleep_ms(1000);
@@ -102,14 +137,23 @@ void task_scheduler() {
     sendResultTask();
 }
 
+/**
+ * \brief Main entry point of the program.
+ * 
+ * This function initializes peripherals and executes tasks in a loop. The loop count can be specified as an argument.
+ * 
+ * \param argc Number of arguments passed to the program.
+ * \param argv Array of arguments passed to the program. The first argument specifies the loop count (0 for infinite loop).
+ * \return Exit status code.
+ */
 int main(int argc, char *argv[]) {
-    init_peripherals();  // Initialiser les périphériques
+    init_peripherals();  // Initialize peripherals
 
     int loop_count = 0;
     bool infinite_loop = true;
 
     if (argc > 1) {
-        loop_count = atoi(argv[1]); // Convertir l'argument en entier
+        loop_count = atoi(argv[1]); // Convert argument to integer
         if (loop_count < 0) {
             printf("Invalid argument. Usage: %s <loop count>.\n", argv[0]);
             return 1;
@@ -118,9 +162,9 @@ int main(int argc, char *argv[]) {
         }
     }
 	
-    while (infinite_loop || loop_count>0) {
+    while (infinite_loop || loop_count > 0) {
         task_scheduler();
         sleep_ms(1000);
-				loop_count--;
+        loop_count--;
     }
 }
